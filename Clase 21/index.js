30803689d105c6be2386d52ceba1d031d3078cfe
Gladys Romero <gladysromero@live.com.ar>
2022-10-23 16:50:36 -0300

    Clase21

---
+++ Clase 21/index.js
@@ -0,0 +1,46 @@
+function buscar(clave) {
+    // alert(`buscando algo en index.js: ${clave}`);
+
+    //data.js > USER_DATA.data
+    //funciones flecha / arrow function
+    const usersFiltered = USERS_DATA.data
+    .filter(user => user.first_name.toLowerCase().includes(clave.toLowerCase()) || user.last_name.toLowerCase().includes(clave.toLowerCase()));
+
+    const htmlUsers = Users(usersFiltered);
+    document.getElementById('users').innerHTML = htmlUsers;
+}
+
+//disparar a las demas funciones
+function render() {
+    console.log('render()');
+
+    //usar al componente Navbar
+    const htmlNavbar = Navbar('Mi primer pseudocomponente',buscar);
+    renderComponent('navbar',htmlNavbar);
+ 
+    search(1);
+}
+
+function search(page) {
+    //ahora invoco a 
+    
+    const url = ` https://reqres.in/api/users${page >=1 ? `?page=${page}` : ''}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const htmlUsers = Users(data.data);
+            renderComponent('users',htmlUsers);
+            
+            //ahora "cargo" el componente Paginator()
+            const htmlPaginator = Paginator(data);
+            renderComponent('paginator',htmlPaginator);
+        }
+    );
+}
+
+function renderComponent(id, htmlComponent) {
+    document.getElementById(id).innerHTML = htmlComponent;
+}
+
+//invocar?
+render();
